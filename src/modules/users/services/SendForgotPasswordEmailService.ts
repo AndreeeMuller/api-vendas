35dc042fdb943132f88a7f1fccd37b22104c9f3b
André Peil Müller <andrepeilmuller@gmail.com>
2021-01-29 01:49:33 -0300

    .

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
+
+interface IRequest {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const userToken = await userTokensRepository.generate(user.id);
+
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebid: ${userToken.token}`,
+    });
+  }
+}
+
+export default SendForgotPasswordEmailService;
